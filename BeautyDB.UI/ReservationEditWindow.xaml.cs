d1a0c19a64694c97326c27a39c717fec80aeb3c0
Dmitry Demin <shepardiwe@gmail.com>
2025-10-06 13:18:12 +0700

    Add bindings from form to Reservation

--- BeautyDB.UI/ReservationEditWindow.xaml.cs
+++ BeautyDB.UI/ReservationEditWindow.xaml.cs
@@ -13,6 +13,7 @@ public partial class ReservationEditWindow : Window
             ? Reservation.Create()
             : reservation.Clone();
         InitializeComponent();
+        DataContext = _reservation;
     }
 
     public static Reservation? Create()
