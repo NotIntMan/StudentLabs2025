1f1696acccc43ab7c91ad0a3c9d252504b5d943d
Dmitry Demin <shepardiwe@gmail.com>
2025-10-06 17:56:56 +0700

    Delete MainWindow, run ReservationsListWindow directly

--- BeautyDB.UI/App.xaml.cs
+++ BeautyDB.UI/App.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows;
+using BeautyDB.Data.InMemory;
+using BeautyDB.Data.Interfaces;
 
 namespace BeautyDB.UI;
 
@@ -7,4 +9,15 @@ namespace BeautyDB.UI;
 /// </summary>
 public partial class App : Application
 {
+    private IReservationRepository _reservationRepository = null!;
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        _reservationRepository = new ReservationRepository();
+
+        var mainWindow = new ReservationsListWindow(_reservationRepository);
+        mainWindow.Show();
+    }
 }
