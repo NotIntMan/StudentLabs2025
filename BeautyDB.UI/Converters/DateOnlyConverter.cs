a11c05a6aa7bd7d39b414706e80f899af1f92022
Dmitry Demin <shepardiwe@gmail.com>
2025-10-06 18:51:45 +0700

    Add second mode to DateOnlyConverter

--- BeautyDB.UI/Converters/DateOnlyConverter.cs
+++ BeautyDB.UI/Converters/DateOnlyConverter.cs
@@ -10,7 +10,12 @@ public class DateOnlyConverter : IValueConverter
     {
         if (value is DateOnly dateOnly)
         {
-            return dateOnly.ToDateTime(TimeOnly.MinValue);
+            switch (parameter as string)
+            {
+                case "DateTime": return dateOnly.ToDateTime(TimeOnly.MinValue);
+            }
+
+            return dateOnly.ToString("dd.MM.yyyy", culture);
         }
 
         return DependencyProperty.UnsetValue;
@@ -18,11 +23,24 @@ public class DateOnlyConverter : IValueConverter
 
     public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
+        if (
+            value is string stringValue
+            && TryParse(stringValue, out var dateOnly)
+        )
+        {
+            return dateOnly;
+        }
+
         if (value is DateTime dateTime)
         {
             return DateOnly.FromDateTime(dateTime);
         }
 
         return DependencyProperty.UnsetValue;
+    }
+
+    public static bool TryParse(string text, out DateOnly dateOnly)
+    {
+        return DateOnly.TryParseExact(text, "dd.MM.yyyy", out dateOnly);
     }
 }
